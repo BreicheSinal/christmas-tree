c0fa1fd773429554d071eaa21828aabd2ae86c50
BreicheSinal <sinalbreiche@gmail.com>
2024-10-27 22:54:59 +0200

    Added endless switching colors of the bg of body

--- scripts/tree.js
+++ scripts/tree.js
@@ -3,19 +3,21 @@ var colorBody = document.getElementById("bodySec");
 
 do{
     var numInput = prompt("ENTER A NUMBER: ");
-    if(numInput < 0 || isNaN(numInput))
-        alert("ENTER A VALID NUMBER GREATER THAN ZERO :)")
+    if(numInput <= 0 || isNaN(numInput))
+        alert("ENTER A NON ZERO 'NUMBER' :)")
     else{
         xmasTree(numInput)
     }
 }
-while (numInput < 0 || isNaN(numInput))
+while (numInput <= 0 || isNaN(numInput))
 
 var colors = ['red', 'green']
+var bgColors = ['white', 'black']
 var index = 0;
 
 function changeColors()
 {
+    bodySec.style.backgroundColor = bgColors[index];    
     bodySec.style.color = colors[index];
     index++;
 
@@ -30,15 +32,19 @@ changeColors();
 function xmasTree(num){
 
     var output = [];
-    console.log("im here");
+    //console.log("im here");
+
+    output.push("*");
+    output.push('\n');
 
     for(var x = 1; x <= num; x++){
         output.push("*");
-        for(var j = 1; j <= (2 * x - 1); j++)
-        {
+        for(var j = 1; j <= (2 * x - 1); j++){
             output.push('|');
-            output.push(' ');
+            for (var y = 0; y < 10; y++)
+                output.push(' ');
         }
+    
         output.push('*');
 
         output.push('\n');
